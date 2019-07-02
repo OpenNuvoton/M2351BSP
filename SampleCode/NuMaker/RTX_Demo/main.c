8ce6d7be639f9813499a0e397525167949c05cff
MS20 WSChang0 <WSChang0@nuvoton.com>
2019-07-02 15:41:26 +0800

    Add KEIL RTX RTOS Sample Code

---
+++ SampleCode/NuMaker/RTX_Demo/main.c
@@ -0,0 +1,210 @@
+/**************************************************************************//**
+ * @file     main.c
+ * @version  V3.00
+ * @brief    Show the usage of clock fail detector and clock frequency monitor function.
+ *
+ * @copyright (C) 2016 Nuvoton Technology Corp. All rights reserved.
+ *
+ ******************************************************************************/
+#include <stdio.h>
+#include "M2351.h"
+#include "cmsis_os2.h"
+
+#define PLL_CLOCK       48000000
+
+
+extern int Init_Thread(void);  //Add this line
+
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  Clock Fail Detector IRQ Handler                                                                        */
+/*---------------------------------------------------------------------------------------------------------*/
+void CLKFAIL_IRQHandler(void)
+{
+    uint32_t u32Reg;
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    u32Reg = CLK->CLKDSTS;
+
+    if(u32Reg & CLK_CLKDSTS_HXTFIF_Msk)
+    {
+        /* HCLK is switched to HIRC automatically if HXT clock fail interrupt is happened */
+        printf("HXT Clock is stopped! HCLK is switched to HIRC.\n");
+
+        /* Disable HXT clock fail interrupt */
+        CLK->CLKDCTL &= ~(CLK_CLKDCTL_HXTFDEN_Msk | CLK_CLKDCTL_HXTFIEN_Msk);
+
+        /* Write 1 to clear HXT Clock fail interrupt flag */
+        CLK->CLKDSTS = CLK_CLKDSTS_HXTFIF_Msk;
+    }
+
+    if(u32Reg & CLK_CLKDSTS_LXTFIF_Msk)
+    {
+        /* LXT clock fail interrupt is happened */
+        printf("LXT Clock is stopped!\n");
+
+        /* Disable LXT clock fail interrupt */
+        CLK->CLKDCTL &= ~(CLK_CLKDCTL_LXTFIEN_Msk | CLK_CLKDCTL_LXTFDEN_Msk);
+
+        /* Write 1 to clear LXT Clock fail interrupt flag */
+        CLK->CLKDSTS = CLK_CLKDSTS_LXTFIF_Msk;
+    }
+
+    if(u32Reg & CLK_CLKDSTS_HXTFQIF_Msk)
+    {
+        /* HCLK should be switched to HIRC if HXT clock frequency monitor interrupt is happened */
+        CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+        printf("HXT Frequency is abnormal! HCLK is switched to HIRC.\n");
+
+        /* Disable HXT clock frequency monitor interrupt */
+        CLK->CLKDCTL &= ~(CLK_CLKDCTL_HXTFQDEN_Msk | CLK_CLKDCTL_HXTFQIEN_Msk);
+
+        /* Write 1 to clear HXT Clock frequency monitor interrupt */
+        CLK->CLKDSTS = CLK_CLKDSTS_HXTFQIF_Msk;
+    }
+
+    /* Lock protected registers */
+    SYS_LockReg();
+
+}
+
+void SYS_Init(void)
+{
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    /* Enable HIRC, HXT and LXT clock */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk);
+    CLK_EnableXtalRC(CLK_PWRCTL_LXTEN_Msk);
+
+    /* Wait for HIRC, HXT and LXT clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
+    CLK_WaitClockReady(CLK_STATUS_LXTSTB_Msk);
+
+    /* Select HCLK clock source as HIRC and HCLK clock divider as 1 */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+
+    /* Set core clock as PLL_CLOCK from PLL */
+    CLK_SetCoreClock(PLL_CLOCK);
+
+    /* Enable UART module clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+
+    /* Select UART module clock source as HIRC and UART module clock divider as 1 */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+    SYS->GPB_MFPH = (SYS->GPB_MFPH & (~(UART0_RXD_PB12_Msk | UART0_TXD_PB13_Msk))) | UART0_RXD_PB12 | UART0_TXD_PB13;
+
+}
+
+
+void UART0_Init()
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init UART                                                                                               */
+    /*---------------------------------------------------------------------------------------------------------*/
+    UART0->LINE = UART_PARITY_NONE | UART_STOP_BIT_1 | UART_WORD_LEN_8;
+    UART0->BAUD = UART_BAUD_MODE2 | UART_BAUD_MODE2_DIVIDER(__HIRC, 115200);
+
+}
+
+static osThreadId_t thd1;
+static osThreadId_t thd2;
+static osThreadId_t thd3;
+
+__NO_RETURN static void MyThread1(void *argument)
+{
+    static uint32_t ticks = 0;
+    
+    (void)argument;
+
+    for(;;)
+    {
+        osThreadFlagsWait(0x0001U, osFlagsWaitAny, 1000);
+        printf("Ticks = %d\n", ticks++);
+    }
+}
+
+
+__NO_RETURN static void MyThread2(void *argument)
+{
+    (void)argument;
+
+    GPIO_SetMode(PA, BIT10, GPIO_MODE_OUTPUT);
+    
+    for(;;)
+    {
+        osThreadFlagsWait(0x0001U, osFlagsWaitAny, 500);
+        PA10 ^= 1;
+    }
+}
+
+
+__NO_RETURN static void MyThread3(void *argument)
+{
+    (void)argument;
+
+    GPIO_SetMode(PA, BIT11, GPIO_MODE_OUTPUT);
+    
+    for(;;)
+    {
+        PA11 = 0;
+        osThreadFlagsWait(0x0001U, osFlagsWaitAny, 2);
+        PA11 = 1;
+        osThreadFlagsWait(0x0001U, osFlagsWaitAny, 98);
+    }
+}
+
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  Main Function                                                                                          */
+/*---------------------------------------------------------------------------------------------------------*/
+int32_t main(void)
+{
+    int32_t i;
+    char ch;
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Init System, peripheral clock and multi-function I/O */
+    SYS_Init();
+
+    /* Init UART5 for printf */
+    UART0_Init();
+
+    printf("RTX Start ...\n");
+    osKernelInitialize();
+
+    /* create thread */
+    thd1 = osThreadNew(MyThread1, NULL, NULL);             
+    if(thd1 == NULL)
+    {
+        printf("Cannot create thread ...\n");
+    }
+
+    thd2 = osThreadNew(MyThread2, NULL, NULL);             
+    if(thd2 == NULL)
+    {
+        printf("Cannot create thread2 ...\n");
+    }
+
+    thd3 = osThreadNew(MyThread3, NULL, NULL);             
+    if(thd2 == NULL)
+    {
+        printf("Cannot create thread3 ...\n");
+    }
+    
+    osKernelStart(); //Start the RTOS
+
+    for(;;){}
+}
