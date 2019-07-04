bfaaba2a48347c087974b520bc09b559e476850f
phtseng <phtseng@nuvoton.com>
2019-07-04 13:08:50 +0800

    Port SecureBootDemo and NSCLibDemo sample code as new BSP style.
    Change-Id: If6910ba28d5939d39003407c4cfe09883c5bb06b

--- SampleCode/MKROM/SecureBootDemo/NuBL2/Keil/NuBL2_startup.s
+++ SampleCode/MKROM/SecureBootDemo/NuBL2/Keil/NuBL2_startup.s
@@ -51,6 +51,7 @@ __heap_limit
                 EXPORT  __Vectors
                 EXPORT  __Vectors_End
                 EXPORT  __Vectors_Size
+                IMPORT  SCU_IRQHandler
                 IMPORT  SendChar_ToUART
                 IMPORT  g_InitialFWinfo
                     
@@ -337,7 +338,7 @@ Default_Handler PROC
                ;EXPORT  SPI5_IRQHandler           [WEAK] ; 96 
                ;EXPORT  DSRC_IRQHandler           [WEAK] ; 97 
                 EXPORT  PDMA1_IRQHandler          [WEAK] ; 98 
-                EXPORT  SCU_IRQHandler            [WEAK] ; 99 
+               ;EXPORT  SCU_IRQHandler            [WEAK] ; 99 
                ;EXPORT  0                         [WEAK] ; 100
                 EXPORT  TRNG_IRQHandler           [WEAK] ; 101
 				
@@ -444,7 +445,7 @@ SDH1_IRQHandler            ; 90
 ;0                          ; 96 
 ;0                          ; 97 
 PDMA1_IRQHandler           ; 98 
-SCU_IRQHandler             ; 99 
+;SCU_IRQHandler             ; 99 
 ;0                         ; 100
 TRNG_IRQHandler            ; 101
 DEFAULT_IRQHandler
