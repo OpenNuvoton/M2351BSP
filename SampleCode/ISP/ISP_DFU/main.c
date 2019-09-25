78609ee491d28930f8735bfa9b85020f5ad779a2
yjchen5 <yjchen5@nuvoton.com>
2019-09-25 10:34:54 +0800

    1. Add ISP_DFU sample code. 2. Update isp_user.c for supporting offset function.
    Change-Id: I4f0f1afa4539436e41af70039e858fcd204ed57a

---
+++ SampleCode/ISP/ISP_DFU/main.c
@@ -0,0 +1,160 @@
+/******************************************************************************//**
+ * @file     main.c
+ * @version  V3.00
+ * @brief
+ *           Demonstrate how to upgrade firmware between USB device and PC through USB DFU (Device Firmware Upgrade) class.
+ *           A Windows tool is also included in this sample code to connect with USB device.
+ *
+ * @copyright (C) 2019 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include "fmc_user.h"
+#include "dfu_transfer.h"
+
+#define TRIM_INIT           (SYS_BASE+0x10C)
+
+#define V6M_AIRCR_VECTKEY_DATA    0x05FA0000UL
+#define V6M_AIRCR_SYSRESETREQ     0x00000004UL
+
+#define DetectPin   PB12
+
+uint32_t g_apromSize;
+
+void ProcessHardFault(void) {}
+void SH_Return(void) {}
+
+uint32_t GetApromSize()
+{
+    //the smallest of APROM size is 2K
+    uint32_t size = 0x800, data;
+    int result;
+
+    do
+    {
+        result = FMC_Read_User(size, &data);
+
+        if (result < 0)
+        {
+            return size;
+        }
+        else
+        {
+            size *= 2;
+        }
+    } while (1);
+}
+
+uint32_t CLK_GetPLLClockFreq(void)
+{
+    return 48000000;
+}
+
+uint32_t CLK_GetCPUFreq(void)
+{
+    return 48000000;
+}
+
+void SYS_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Enable Internal RC 48MHz clock */
+    CLK->PWRCTL |= CLK_PWRCTL_HIRC48EN_Msk;
+
+    /* Waiting for Internal RC clock ready */
+    while(!(CLK->STATUS & CLK_STATUS_HIRC48STB_Msk));
+
+    /* Switch HCLK clock source to Internal RC and HCLK source divide 1 */
+    CLK->CLKSEL0 = (CLK->CLKSEL0 & (~CLK_CLKSEL0_HCLKSEL_Msk)) | CLK_CLKSEL0_HCLKSEL_HIRC48;
+    CLK->CLKDIV0 = (CLK->CLKDIV0 & (~CLK_CLKDIV0_HCLKDIV_Msk)) | CLK_CLKDIV0_HCLK(1);
+    /* Use HIRC48 as USB clock source */
+    CLK->CLKSEL0 = (CLK->CLKSEL0 & (~CLK_CLKSEL0_USBSEL_Msk)) | CLK_CLKSEL0_USBSEL_HIRC48;
+    CLK->CLKDIV0 = (CLK->CLKDIV0 & (~CLK_CLKDIV0_USBDIV_Msk)) | CLK_CLKDIV0_USB(1);
+    /* Select USBD */
+    SYS->USBPHY = (SYS->USBPHY & ~SYS_USBPHY_USBROLE_Msk) | SYS_USBPHY_OTGPHYEN_Msk | SYS_USBPHY_SBO_Msk;
+    /* Enable IP clock */
+    CLK->APBCLK0 |= CLK_APBCLK0_USBDCKEN_Msk;
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* USBD multi-function pins for VBUS, D+, D-, and ID pins */
+    SYS->GPA_MFPH &= ~(SYS_GPA_MFPH_PA12MFP_Msk | SYS_GPA_MFPH_PA13MFP_Msk | SYS_GPA_MFPH_PA14MFP_Msk | SYS_GPA_MFPH_PA15MFP_Msk);
+    SYS->GPA_MFPH |= (SYS_GPA_MFPH_PA12MFP_USB_VBUS | SYS_GPA_MFPH_PA13MFP_USB_D_N | SYS_GPA_MFPH_PA14MFP_USB_D_P | SYS_GPA_MFPH_PA15MFP_USB_OTG_ID);
+}
+
+void USBD_IRQHandler(void);
+/*---------------------------------------------------------------------------------------------------------*/
+/*  Main Function                                                                                          */
+/*---------------------------------------------------------------------------------------------------------*/
+int32_t main(void)
+{
+    uint32_t u32TrimInit;
+
+    /* Unlock write-protected registers */
+    SYS_UnlockReg();
+
+    /* Init system and multi-funcition I/O */
+    SYS_Init();
+
+    CLK->AHBCLK |= CLK_AHBCLK_ISPCKEN_Msk;
+    FMC->ISPCTL |= FMC_ISPCTL_ISPEN_Msk | FMC_ISPCTL_APUEN_Msk | FMC_ISPCTL_ISPFF_Msk;
+    g_apromSize = GetApromSize();
+
+    /* Open USB controller */
+    USBD_Open(&gsInfo, DFU_ClassRequest, NULL);
+
+    /*Init Endpoint configuration for DFU */
+    DFU_Init();
+
+    /* Start USB device */
+    USBD_Start();
+
+    /* Backup default trim */
+    u32TrimInit = M32(TRIM_INIT);
+    /* Clear SOF */
+    USBD->INTSTS = USBD_INTSTS_SOFIF_Msk;
+
+    /* polling USBD interrupt flag */
+    while(DetectPin == 0)
+    {
+        /* Start USB trim if it is not enabled. */
+        if((SYS->TCTL48M & SYS_TCTL48M_FREQSEL_Msk) != 1)
+        {
+            /* Start USB trim only when SOF */
+            if(USBD->INTSTS & USBD_INTSTS_SOFIF_Msk)
+            {
+                /* Clear SOF */
+                USBD->INTSTS = USBD_INTSTS_SOFIF_Msk;
+                /* Re-enable crystal-less */
+                SYS->TCTL48M = 0x01;
+                SYS->TCTL48M |= SYS_TCTL48M_REFCKSEL_Msk;
+            }
+        }
+
+        /* Disable USB Trim when error */
+        if(SYS->TISTS48M & (SYS_TISTS48M_CLKERRIF_Msk | SYS_TISTS48M_TFAILIF_Msk))
+        {
+            /* Init TRIM */
+            M32(TRIM_INIT) = u32TrimInit;
+            /* Disable crystal-less */
+            SYS->TCTL48M = 0;
+            /* Clear error flags */
+            SYS->TISTS48M = SYS_TISTS48M_CLKERRIF_Msk | SYS_TISTS48M_TFAILIF_Msk;
+            /* Clear SOF */
+            USBD->INTSTS = USBD_INTSTS_SOFIF_Msk;
+        }
+
+        USBD_IRQHandler();
+    }
+
+    SYS->RSTSTS = (SYS_RSTSTS_PORF_Msk | SYS_RSTSTS_PINRF_Msk);//clear bit
+    FMC->ISPCTL &=  ~(FMC_ISPCTL_ISPEN_Msk | FMC_ISPCTL_BS_Msk);
+    SCB->AIRCR = (V6M_AIRCR_VECTKEY_DATA | V6M_AIRCR_SYSRESETREQ);
+
+    /* Trap the CPU */
+    while(1);
+}
+
+/*** (C) COPYRIGHT 2019 Nuvoton Technology Corp. ***/
