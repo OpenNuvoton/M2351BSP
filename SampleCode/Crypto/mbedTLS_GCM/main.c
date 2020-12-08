88f591e7f24372d18be3c0ceed74d54b2c1165ea
SFWU <SFWU@nuvoton.com>
2020-12-08 16:56:05 +0800

    Add mbedtls GCM sample code for AES-GCM test.
    Change-Id: I9b3d6e675d4c92540248c71343c2409407acddde

---
+++ SampleCode/Crypto/mbedTLS_GCM/main.c
@@ -0,0 +1,112 @@
+/******************************************************************************
+ * @file     main.c
+ * @version  V3.00
+ * $Revision: 3 $
+ * $Date: 20/12/08 $
+ * @brief    Show how mbedTLS GCM function works.
+ * @note
+ * Copyright (C) 2020 Nuvoton Technology Corp. All rights reserved.
+*****************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+
+
+#define MBEDTLS_EXIT_SUCCESS    0
+#define MBEDTLS_EXIT_FAILURE    -1
+
+void SYS_Init(void);
+
+void SYS_Init(void)
+{
+    /* Set PF multi-function pins for XT1_OUT(PF.2) and XT1_IN(PF.3) */
+    SYS->GPF_MFPL = (SYS->GPF_MFPL & (~SYS_GPF_MFPL_PF2MFP_Msk)) | SYS_GPF_MFPL_PF2MFP_XT1_OUT;
+    SYS->GPF_MFPL = (SYS->GPF_MFPL & (~SYS_GPF_MFPL_PF3MFP_Msk)) | SYS_GPF_MFPL_PF3MFP_XT1_IN;
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    /* Enable HIRC clock */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+
+    /* Wait for HIRC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+
+    /* Select HCLK clock source as HIRC and HCLK source divider as 1 */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+
+    /* Enable HXT clock */
+    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk);
+
+    /* Wait for HXT clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
+
+    /* Enable PLL */
+    CLK->PLLCTL = CLK_PLLCTL_128MHz_HIRC;
+
+    /* Waiting for PLL stable */
+    CLK_WaitClockReady(CLK_STATUS_PLLSTB_Msk);
+
+    /* Select HCLK clock source as PLL and HCLK source divider as 2 */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_PLL, CLK_CLKDIV0_HCLK(2));
+
+    /* Enable UART module clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+
+    /* Select UART module clock source as HXT and UART module clock divider as 1 */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HXT, CLK_CLKDIV0_UART0(1));
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Set multi-function pins for UART0 RXD and TXD */
+    SYS->GPB_MFPH = (SYS->GPB_MFPH & (~(UART0_RXD_PB12_Msk | UART0_TXD_PB13_Msk))) | UART0_RXD_PB12 | UART0_TXD_PB13;
+}
+
+extern int GCMTest(void);
+
+int32_t main(void)
+{
+    int  i32Ret = MBEDTLS_EXIT_SUCCESS;
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Init System, IP clock and multi-function I/O. */
+    SYS_Init();
+
+    /* Lock protected registers */
+    SYS_LockReg();
+
+    /* Configure UART0: 115200, 8-bit word, no parity bit, 1 stop bit. */
+    UART_Open(UART0, 115200);
+
+    /*
+        This sample code is used to show how to use StdDriver API to implement ISP functions.
+    */
+
+    printf("\n\n");
+    printf("+--------------------------------+\n");
+    printf("|        GCM Sample Code         |\n");
+    printf("+--------------------------------+\n");
+
+    printf("\n GCM test start...\n\n");
+    i32Ret = GCMTest();
+    printf("\n GCM test done ...\n");
+
+
+    if(i32Ret == MBEDTLS_EXIT_SUCCESS)
+    {
+        printf("\nTest OK\n");
+    }
+    else
+    {
+        printf("\nTest fail\n");
+    }
+
+    while(1);
+
+}
+/*** (C) COPYRIGHT 2019 Nuvoton Technology Corp. ***/
+
+
