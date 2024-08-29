690e4bd0f9ede8a3c590eddd27e71d990e2b3e2b
WSChang0 <WSChang0@nuvoton.com>
2024-08-29 10:33:51 +0800

    Remove Keil MDK component to improve compatibility
    Change-Id: I10636abcb94242e5dab124d5d5db0ae0d36b014f

---
+++ SampleCode/NuMaker/RTX_Demo/Keil/RTE/_RTX_Demo/RTE_Components.h
@@ -0,0 +1,24 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'RTX_Demo' 
+ * Target:  'RTX_Demo' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "M2351.h"
+
+/*  ARM::CMSIS:RTOS2:Keil RTX5:Library:5.5.4 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+
+
+#endif /* RTE_COMPONENTS_H */
